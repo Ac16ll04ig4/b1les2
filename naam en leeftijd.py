24f9ec14a09f542479007928381d10189745d9e5
Ac16ll04ig4 <ameriecallegari@hotmail.com>
2024-09-28 10:44:47 +0200

    Update naam en leeftijd.py

--- naam en leeftijd.py
+++ naam en leeftijd.py
@@ -3,3 +3,13 @@ def vraag_naam_en_leeftijd():
     leetijd= input("Wat is jouw leeftijd?")
     print("je naam is", naam,"en je leeftijd is", leeftijd,",")
 vraag_naam_en_leeftijd()
+
+def vraag_input()
+    naam= input("Geef me een naam:")
+    plaats= input("Geef me een plaats:")
+    dier= input("Geef me een dier:")
+
+
+
+def zegWelkom( naam="world"):
+    print("Welkom bij de Verhaalgenerator!")
